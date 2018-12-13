7b6742df52eebcc600c54c515201617ab625bb38
Cameron Devine <camerondtrain@gmail.com>
2018-12-12 16:28:29 -0800

    fixed a couple namespace issues in the robot file

--- robot/robot.h
+++ robot/robot.h
@@ -1,10 +1,9 @@
 CREulerMode convention = CR_EULER_MODE_XYZ;
 
-std::vector<CR::CRFrameEuler*> frames = {
+std::vector<CRFrameEuler*> frames = {
 	new CRFrameEuler(0, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_G),
 	new CRFrameEuler(0, 0, 89.2, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
 	new CRFrameEuler(425, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
 	new CRFrameEuler(392, 109.3, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
 	new CRFrameEuler(94.75, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A),
 	new CRFrameEuler(0, 82.5, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B)};
-
