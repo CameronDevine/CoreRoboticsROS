efa6bfd3d947b219e0bce6016aa8b876306c93c7
Cameron Devine <camerondtrain@gmail.com>
2018-12-14 17:25:15 -0800

    tried to fix the robot kinematics

--- robot/robot.h
+++ robot/robot.h
@@ -4,6 +4,6 @@ std::vector<CRFrameEuler*> frames = {
 	new CRFrameEuler(0, 0, 89.2, 0, 0, 0, convention, CR_EULER_FREE_ANG_G),
 	new CRFrameEuler(425, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
 	new CRFrameEuler(392, 109.3, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
-	new CRFrameEuler(94.75, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
+	new CRFrameEuler(0, 0, -94.75, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
 	new CRFrameEuler(0, 82.5, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A),
 	new CRFrameEuler(0, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B)};
