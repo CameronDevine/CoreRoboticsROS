f89fa4d2f53836beee63db4a88fe2e94bbf27f2b
Cameron Devine <camerondtrain@gmail.com>
2019-01-10 16:49:30 -0800

    got the first three joints working correctly

--- robot/robot.h
+++ robot/robot.h
@@ -1,9 +1,9 @@
 //CREulerMode convention = CR_EULER_MODE_XYZ;
 
 std::vector<CRFrameEuler*> frames = {
-	new CRFrameEuler(0, 0, 89.2, 0, 0, 0, convention, CR_EULER_FREE_ANG_G),
-	new CRFrameEuler(0, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A),
-	new CRFrameEuler(0, 425, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A),
-	new CRFrameEuler(109.3, 392, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A),
-	new CRFrameEuler(0, 0, -94.75, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
-	new CRFrameEuler(82.5, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_A)};
+	new CRFrameEuler(0, 0, 128, 0, 0, 0, convention, CR_EULER_FREE_ANG_G),
+	new CRFrameEuler(0, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
+	new CRFrameEuler(612.7, 0, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B),
+	new CRFrameEuler(571.6, 163.9, 0, M_PI, 0, 0, convention, CR_EULER_FREE_ANG_B),
+	new CRFrameEuler(0, 0, -115.7, 0, 0, 0, convention, CR_EULER_FREE_ANG_G),
+	new CRFrameEuler(0, -92.2, 0, 0, 0, 0, convention, CR_EULER_FREE_ANG_B)};
